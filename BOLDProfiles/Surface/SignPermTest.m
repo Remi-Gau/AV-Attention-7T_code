7dfcd3202723445ace60dff431135e6f8220447c
Remi Gau <remi_gau@hotmail.com>
2018-06-29 14:46:40 +0200

    Scripts for surface based estimation of BOLD laminar profiles

---
+++ BOLDProfiles/Surface/SignPermTest.m
@@ -0,0 +1,19 @@
+function P = SignPermTest(tmp)
+sets = {};
+for iSub=1:11
+    sets{iSub} = [-1 1]; %#ok<*AGROW>
+end
+[a, b, c, d, e, f, g, h, i, j, k] = ndgrid(sets{:});
+ToPermute = [a(:), b(:), c(:), d(:), e(:), f(:), g(:), h(:), i(:), j(:), k(:)];
+
+
+for iPerm = 1:size(ToPermute,1)
+    tmp2 = ToPermute(iPerm,:);
+    tmp2 = repmat(tmp2',1,size(tmp,2));
+    Perms(iPerm,:) = mean(tmp.*tmp2);  %#ok<*AGROW>
+end
+P = sum( ...
+    abs( Perms ) > ...
+    repmat( abs(mean(tmp)), size(Perms,1),1)  ) ...
+    / size(Perms,1);
+end
---
+++ BOLDProfiles/Surface/TransferMapping.m
@@ -0,0 +1,46 @@
+clc
+clear
+close all
+
+StartDirectory = fullfile(pwd, '..', '..', '..');
+
+addpath(genpath(fullfile(StartDirectory, 'SubFun')))
+Get_dependencies('D:\Dropbox')
+
+SubjectList = [...
+    '02';...
+    '03';...
+    '04';...
+    '07';...
+    '08';...
+    '09';...
+    '11';...
+    '12';...
+    '13';...
+    '15';...
+    '16'
+    ];
+
+Source_dir = 'C:\Users\Remi\Documents';
+
+for  iSubj = 1:size(SubjectList,1)
+    SubjID = SubjectList(iSubj,:);
+    
+    T1 = spm_select('FPList', Source_dir, ...
+        ['^T1_' SubjID '.*_lcr_gm_avg_inf_qT1.vtk$']);
+    
+    Files2transfer = spm_select('FPList', Source_dir, ...
+        ['^Subj_' SubjID '_lcr_.*.vtk$']);
+
+    
+    [inf_vertex,inf_faces,T1] = read_vtk(T1, 0, 1);
+    
+    for  iFile = 1:size(Files2transfer,1)
+        
+        Filename = deblank(Files2transfer(iFile,:));
+        
+        [inf_vertex_func,inf_faces_func,mapping] = read_vtk(Filename, 0, 1);
+        
+        write_vtk([Filename(1:end-4) '_remap.vtk'], inf_vertex, inf_faces, mapping)
+    end
+end
