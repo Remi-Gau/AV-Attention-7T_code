02b1bc632fb4ee5edb939cd944c5addd7de7c278
Remi Gau <remi_gau@hotmail.com>
2019-04-21 18:22:03 +0200

    update normalize script: keep high-res

--- PreProcess/Preprocess_04_Normalize.m
+++ PreProcess/Preprocess_04_Normalize.m
@@ -6,28 +6,21 @@ function Preprocess_04_Normalize
 clear 
 clc
 
-
-spm_jobman('initcfg')
-spm_get_defaults;
-global defaults %#ok<NUSED>
-
-
 %  Folders definitions
-% RootFolder = fullfile(pwd, '..', '..');
-RootFolder = '/media/rxg243/BackUp2/AV_Integration_7T_2';
+RootFolder = '/media/remi/BackUp2/AV_Integration_7T_2';
 
 SubjectList = [...
     '02';...
     '03';...
     '04';...
-    '06';...
+%     '06';...
     '07';...
     '08';...
     '09';...
     '11';...
     '12';...
     '13';...
-    '14';...
+%     '14';...
     '15';...
     '16'
     ];
@@ -36,20 +29,19 @@ FoldersNames = {...
     1:4;...
     1:4;...
     1:4;...
-    1:2;...
+%     1:2;...
     1:4;...
     1:4;...
     1:4;...
     1:4;...
     1:4;...
     1:4;...
-    1:2;...
+%     1:2;...
     1:4;...
     1:4;...
     };
 
-for SubjInd = 1:size(SubjectList,1)
-    
+for SubjInd = 2:size(SubjectList,1)
     
     SubjID = SubjectList(SubjInd,:)
     
@@ -57,29 +49,30 @@ for SubjInd = 1:size(SubjectList,1)
     
     NiftiSourceFolder = fullfile(SubjectFolder, 'Nifti', 'NoMoCo');
     
-    
     NbRuns = length(FoldersNames{SubjInd});
-    
     
     matlabbatch = {};
     
-    matlabbatch{1}.spm.spatial.normalise.write.subj.def{1} = fullfile(SubjectFolder, 'FFX', 'Structural', 'y_UNI.nii');
+    matlabbatch{1}.spm.spatial.normalise.write.subj.def{1} = ...
+        fullfile(SubjectFolder, 'Structural', 'SPM_Segmentation', 'y_UNI.nii');
     
     % List the images for each run and the mean image 
     for RunInd=1:NbRuns
         
-        cd(fullfile(NiftiSourceFolder, sprintf('%2.2d', FoldersNames{SubjInd}(RunInd))))
-        
+        RunFolder = fullfile(NiftiSourceFolder, sprintf('%2.2d', FoldersNames{SubjInd}(RunInd)));
+
         if RunInd==1
-            TEMP = dir('mean*.nii');
-            matlabbatch{1}.spm.spatial.normalise.write.subj.resample{1,1} = fullfile(NiftiSourceFolder, '01', TEMP.name);
+            TEMP = dir(fullfile(RunFolder,'meanUR*.nii'));
+            matlabbatch{1}.spm.spatial.normalise.write.subj.resample{1,1} = ...
+                fullfile(RunFolder, TEMP.name);
         end
        
-        TEMP = dir('URS*.nii');
-        TEMP = spm_vol(TEMP.name);
+        TEMP = dir(fullfile(RunFolder,'URS*.nii'));
+        TEMP = spm_vol(fullfile(RunFolder, TEMP.name));
         
         for i=1:length(TEMP)
-            matlabbatch{1}.spm.spatial.normalise.write.subj.resample{end+1,1} = [fullfile(pwd,TEMP(i).fname) ,',', num2str(i)];
+            matlabbatch{1}.spm.spatial.normalise.write.subj.resample{end+1,1} = ...
+                [TEMP(i).fname ,',', num2str(i)];
         end
         
         
@@ -87,13 +80,13 @@ for SubjInd = 1:size(SubjectList,1)
     
     char(matlabbatch{1}.spm.spatial.normalise.write.subj.resample)
     
-    matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70;...
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [...
+        -78 -112 -70;...
         78 76 85];
-    matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1.5 1.5 1.5];
+    matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [.75 .75 .75];
     matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
     
-    cd(fullfile(SubjectFolder, 'FFX', 'Structural'))
-    SaveBatch('Normalize_EPI_Subj_',SubjID, matlabbatch)
+    SaveBatch(fullfile(SubjectFolder,'Normalize_EPI_Subj_'), SubjID, matlabbatch)
     
     
     fprintf('\n')
@@ -103,19 +96,19 @@ for SubjInd = 1:size(SubjectList,1)
     
     spm_jobman('run',matlabbatch)
     
-    matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {fullfile(SubjectFolder, 'FFX', 'Structural', 'mUNI.nii')};
-    SaveBatch('Normalize_Struc_Subj_',SubjID, matlabbatch)
-    
-    spm_jobman('run',matlabbatch)
-    
-    cd (RootFolder)
-    
-end
+%     matlabbatch{1}.spm.spatial.normalise.write.subj.resample = ...
+%         {fullfile(SubjectFolder, 'Structural', 'SPM_Segmentation', 'mUNI.nii')};
+%     
+%     SaveBatch(fullfile(SubjectFolder, 'Normalize_Struc_Subj_'), SubjID, matlabbatch)
+%     
+%     spm_jobman('run',matlabbatch)
 
 end
 
-function SaveBatch(Prefix,SubjID, matlabbatch)
+end
+
+function SaveBatch(Prefix, SubjID, matlabbatch)
 DateFormat = 'yyyy_mm_dd_HH_MM';
-save (strcat(Prefix, SubjID, '_', datestr(now, DateFormat), '_matlabbatch'), 'matlabbatch');
+save (strcat(Prefix, SubjID, '_', datestr(now, DateFormat), '_matlabbatch.mat'), 'matlabbatch');
 end
 
