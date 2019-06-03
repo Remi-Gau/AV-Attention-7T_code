be28ce204fcdb89bc75ef92e3359b1515e6ba90e
Remi Gau <remi_gau@hotmail.com>
2019-06-03 22:46:38 +0200

    allow for smoothness estimation of raw 4D bold data

--- QC/smoothness_estimation.sh
+++ QC/smoothness_estimation.sh
@@ -9,20 +9,39 @@
 #-v /media/remi/BackUp2/AV_Integration_7T_2/derivatives/:/out \
 #afni/afni
 
+# docker run -it --rm \
+# -v '/home/remi/github/AV-Attention-7T_code':/code \
+# -v /media/remi/BIDS/AV_Att/rawdata:/data:ro \
+# -v ~/:/out \
+# afni/afni
+
 
 Subjects_List='02 03 04 06 07 08 09 11 12 13 14 15 16'
 
 for Subject in $Subjects_List;
 do
+  file_ls=`ls /data/sub-$Subject/*/func/*attention*bold.nii.gz`
 
-  mkdir /out/afni/
-  mkdir /out/afni/sub-$Subject
-  mkdir /out/afni/sub-$Subject/ses-1
-  mkdir /out/afni/sub-$Subject/ses-1/func
+  # echo $file_ls
 
-  3dFWHMx -automask \
-  -out /out/afni/sub-$Subject/ses-1/func/FWHM_meanUR.txt \
-  -input /data/sub-$Subject/ses-1/func/meanURsub-${Subject}_ses-1_task-audiovisualattention_run-01_bold.nii
+  # mkdir /out/afni/
+  # mkdir /out/afni/sub-$Subject
+
+  for File in $file_ls;
+  do
+
+    FileName="$(echo $File | cut -d/ -f 6 | cut -d. -f 1)"
+
+    # echo $FileName
+
+    3dFWHMx -automask \
+    -out /out/afni/sub-$Subject/FWHM_${FileName}.txt \
+    -input $File
+  done
+
+  #3dFWHMx -automask \
+  #-out /out/afni/sub-$Subject/ses-1/func/FWHM_meanUR.txt \
+  #-input /data/sub-$Subject/ses-1/func/meanURsub-${Subject}_ses-1_task-audiovisualattention_run-01_bold.nii
 
   #3dFWHMx -automask \
   #-out /out/afni/sub-$Subject/ses-1/func/FWHM_r4meanUR.txt \
