f6378176dd5bd1307f6ef4274a77d278019b2d8f
Remi Gau <remi_gau@hotmail.com>
2019-07-19 10:52:54 +0200

    refactor display lmm results and implement decision tree

--- BOLDProfiles/display_lmm_results.m
+++ BOLDProfiles/display_lmm_results.m
@@ -1,10 +1,21 @@
-% small script to print out the results of the LMM
+function display_lmm_results()% small script to print out the results of the LMM
 
 clear
 close all
 clc
 
-% Models
+DO = 2; % 1 BOLD ; 2 MVPA
+
+CodeFolder = 'D:\github\AV-Attention-7T_code';
+FigureFolder = fullfile(CodeFolder, 'Figures');
+
+% in case it is not running on remi's computer
+if ~exist(FigureFolder, 'dir')
+    FigureFolder = pwd;
+end
+
+
+% Models BOLD
 % 1 - [A-fix]_{Att_A, Att_V} - A1 - PT
 % 4 - [V-fix]_{Att_A, Att_V} - V1 - V2-3
 % 3 - [V-fix]_{Att_A, Att_V} - A1 - PT
@@ -13,6 +24,13 @@ clc
 % 8 - [AV - V]_{Att_A, Att_V} - V1 - V2-3
 % 9 - [Att_V - Att_A]_{A, V, AV} - A1 - PT
 % 10 - [Att_V - Att_A]_{A, V, AV} - V1 - V2-3
+
+% Models MVPA
+% 1 - [AV VS A]_{att A, att V} - A1 - PT
+% 4 - [AV VS V]_{att A, att V} - V1 - V2-3
+% 5 - [Att_A VS Att_V]_{A, V, AV} - A1 - PT
+% 6 - [Att_A VS Att_V]_{A, V, AV} - V1 - V2-3
+
 
 % call :
 % lme = fitlmematrix(x, Y, Z, G, 'FitMethod', 'REML',...
@@ -23,12 +41,16 @@ clc
 %     'RandomEffectGroups',...
 %     {'Subject'});
 
+switch DO
+    case 1
+        load(fullfile(FigureFolder, 'LMM_BOLD_results.mat'), 'models');
+        model_of_interest = [1 4 3 2 5 8 9 10];
+    case 2
+        load(fullfile(FigureFolder, 'LMM_MVPA_results.mat'), 'models');
+        model_of_interest = [1 4:6];
+end
 
-% CodeFolder = 'D:\github\AV-Attention-7T_code';
-% FigureFolder = fullfile(CodeFolder, 'Figures');
-% load(fullfile(FigureFolder, 'LMM_results.mat'), 'models');
 
-load('LMM_results.mat', 'models');
 
 
 
@@ -66,7 +88,6 @@ title('random effect group')
 
 
 %% print out results
-model_of_interest = [1 4 3 2 5 8 9 10];
 pattern = '%s\t F(%i,%i)= %f\t p = %f\n';
 
 for i_model = model_of_interest %1:numel(models)
@@ -74,38 +95,63 @@ for i_model = model_of_interest %1:numel(models)
     model = models(i_model);
     
     % print out results
-    fprintf('%s %i - %s - %s', '%', i_model, model.name, model.ROIs)
-    disp(model.lme)
+    %     fprintf('%s %i - %s - %s', '%', i_model, model.name, model.ROIs)
+    %     disp(model.lme)
     
     % results from some specific contrasts
-    
-    % effect of CST
     fprintf('%s %i - %s - %s\n', '%', i_model, model.name, model.ROIs)
-    c = [1 0 1 0];
-    [PVAL,F,DF1,DF2] = coefTest(model.lme, c);
-    fprintf(pattern, ...
-        'effect of CST', ...
-        DF1, DF2, ...
-        F, PVAL);
-    
-    % effect of LIN
-    c = [0 1 0 1];
-    [PVAL,F,DF1,DF2] = coefTest(model.lme, c);
-    fprintf(pattern, ...
-        'effect of LIN', ...
-        DF1, DF2, ...
-        F, PVAL);
-    
-    % effect of either
+    % effect of either linear or constant in either ROIs
     c = [...
-        1 0 1 0 ;...
-        0 1 0 1 ];
-    [PVAL,F,DF1,DF2] = coefTest(model.lme, c);
-    fprintf(pattern, ...
-        'effect of either CST/LIN', ...
-        DF1, DF2, ...
-        F, PVAL);
+        1 0 0 0 ;...
+        0 1 0 0 ;...
+        0 0 1 0 ;...
+        0 0 0 1 ];
+    message = 'effect of either linear or constant in either ROI';
+    PVAL = test_and_print(model, c, pattern, message);
     
+    if PVAL>.05
+        c = [...
+            1 0 0 0 ;...
+            0 1 0 0];
+        message = 'effect of either CST/LIN in ROI 1';
+        PVAL = test_and_print(model, c, pattern, message);
+        
+        c = [...
+            0 0 1 0 ;...
+            0 0 0 1];
+        message = 'effect of either CST/LIN in ROI 2';
+        PVAL = test_and_print(model, c, pattern, message);
+    else
+        % effect of either CST/LIN
+        c = [...
+            1 0 1 0 ;...
+            0 1 0 1 ];
+        message = 'effect of either CST/LIN';
+        PVAL = test_and_print(model, c, pattern, message);
+        
+        if PVAL>.05
+            % effect of CST
+            c = [1 0 1 0];
+            message = 'effect of CST';
+            PVAL = test_and_print(model, c, pattern, message);
+            
+            % effect of LIN
+            c = [0 1 0 1];
+            message = 'effect of LIN';
+            PVAL = test_and_print(model, c, pattern, message);
+        end
+    end
     
     fprintf('\n\n\n')
+end
+
+end
+
+
+function [PVAL,F,DF1,DF2] = test_and_print(model, c, pattern, message)
+[PVAL,F,DF1,DF2] = coefTest(model.lme, c);
+fprintf(pattern, ...
+    message, ...
+    DF1, DF2, ...
+    F, PVAL);
 end
